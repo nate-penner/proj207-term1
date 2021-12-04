e82cf8c0a0b5027356f3f5e8595bfcdb80585102
tarapoto2006 <32444403+tarapoto2006@users.noreply.github.com>
2021-12-03 19:49:46 -0700

    Added scripts to install/uninstall a binary. Removed menu.js

---
+++ install.js
@@ -0,0 +1,16 @@
+const colors = require('colors');
+const {spawn} = require('child_process');
+const platform = process.platform;
+
+const npmCmd = (platform === 'win32' ? 'npm.cmd' : 'npm');
+const npm = spawn(npmCmd, ['install', '-g']);
+
+npm.stdout.on('data', (data) => {
+    console.log(`${new Date(Date.now()).toISOString()} - [${colors.brightGreen('travelexperts')}] - ${data}`);
+});
+npm.stderr.on('data', (data) => {
+    console.error(`${new Date(Date.now()).toISOString()} - [${colors.brightGreen('travelexperts')}] - ${data}`);
+});
+npm.on('close', (code) => {
+    console.error(`${new Date(Date.now()).toISOString()} - [${colors.brightGreen('travelexperts')}] - npm exited with code ${code}`);
+});
